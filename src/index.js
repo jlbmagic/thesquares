be444b581589c758b786a158a1686e24c7d16f18
Jeremy Brown <jeremybrown@jeremys-mbp.lan>
2019-05-10 08:09:27 -0400

    added filter mechanism

--- src/index.js
+++ src/index.js
@@ -2,10 +2,42 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+const things = [
+  { id: 1, value: "WorkOut", color: "#403556" },
+  { id: 2, value: "Work", color: "#48f442" },
+  { id: 3, value: "Video Games", color: "#f41f8d" },
+  { id: 4, value: "Video Games", color: "#f41f8d" },
+  { id: 5, value: "WorkOut", color: "#403556" },
+  { id: 6, value: "Work", color: "#48f442" },
+  { id: 7, value: "Video Games", color: "#f41f8d" },
+  { id: 8, value: "Work", color: "#48f442" },
+  { id: 9, value: "WorkOut", color: "#403556" },
+  { id: 10, value: "Video Games", color: "#f41f8d" },
+  { id: 11, value: "Video Games", color: "#f41f8d" },
+  { id: 12, value: "Read", color: "#f4bc42" }
+];
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null
+    };
+    this.removeItem = this.removeItem.bind(this);
+  }
+  removeItem(e) {
+    // console.log(e);
+    this.props.removeSquare(e);
+    // console.log(this.state.data.length);
+  }
   render() {
+    // console.log(this.props.id);
     return (
-      <button className="box" style={{ backgroundColor: this.props.color }}>
+      <button
+        className="box"
+        onClick={() => this.removeItem(this.props.id)}
+        style={{ backgroundColor: this.props.color }}
+        // key={this.props.id}
+      >
         {this.props.value}
       </button>
     );
@@ -34,42 +66,57 @@ class CreateSquares extends React.Component {
       return array;
     }
     const theArray = this.props.list;
+    const remove = this.props.removeSquare;
+    // console.log(remove);
     const things = shuffle(theArray);
-    console.log(things);
+    // console.log(things);
     const rows = things.map(function(ele, i) {
       var v = ele.value;
-      console.log(v);
-      return <Square key={i} value={v} color={ele.color} />;
+      // console.log(v);
+      return (
+        <Square
+          key={ele.id}
+          id={ele.id}
+          value={v}
+          color={ele.color}
+          removeSquare={remove}
+        />
+      );
       // return <h1>Hi</h1>;
     });
-    console.log(rows);
+    // console.log(rows);
     return rows;
   }
 }
 class Game extends React.Component {
-  renderSquare(i) {
-    return <Square value={i} />;
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: things
+    };
+    this.removeSquare = this.removeSquare.bind(this);
+  }
+  removeSquare(square) {
+    // console.log("Remove " + square);
+    // console.log(this.state.data);
+    this.setState({
+      data: this.state.data.filter(el => el.id !== square)
+    });
+    // console.log(this.state.data.length);
   }
 
   render() {
-    const things = [
-      { value: "WorkOut", color: "#403556" },
-      { value: "Video Games", color: "#f41f8d" },
-      { value: "Work", color: "#48f442" },
-      { value: "Video Games", color: "#f41f8d" },
-      { value: "WorkOut", color: "#403556" },
-      { value: "Work", color: "#48f442" },
-      { value: "Video Games", color: "#f41f8d" },
-      { value: "Work", color: "#48f442" },
-      { value: "WorkOut", color: "#403556" },
-      { value: "Video Games", color: "#f41f8d" },
-      { value: "Video Games", color: "#f41f8d" },
-      { value: "Read", color: "#f4bc42" }
-    ];
+    // console.log(this.state.data);
+
     return (
-      <div className="wrapper">
-        {/* {this.renderSquare(things[4])} */}
-        <CreateSquares list={things} />
+      <div>
+        <h1>My Time Allocation</h1>
+        <div className="wrapper">
+          <CreateSquares
+            list={this.state.data}
+            removeSquare={this.removeSquare}
+          />
+        </div>
       </div>
     );
   }
